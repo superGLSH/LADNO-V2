9412a1ccbf2895e130f47d0b25d01ac923dcff78
superGLSH <glebshu@gmail.com>
2021-11-22 19:04:36 +0300

    10
    new version

--- main.py
+++ main.py
@@ -1,38 +1,35 @@
 ﻿from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow, QLabel, QGridLayout
 from PyQt5.QtGui import QPainter, QPixmap, QPen, QColor
 from PyQt5.QtCore import Qt
-from PyQt5 import uic
+from PyQt5 import uic, QtWidgets
 from random import randint
+from UI import Ui
+import sys
+
+app = QtWidgets.QApplication(sys.argv)
+mw = QtWidgets.QMainWindow()
+ui = Ui()
+ui.setupUi(mw)
+mw.show()
+
+ui.label.setFixedSize(400, 400)
+ui.label.setPixmap(QPixmap(400, 400))
+ui.label.move(100, 100)
 
 
-class Test(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        uic.loadUi('UI.ui', self)
-
-        self.pushButton.clicked.connect(self.circle)
-
-        self.label.setFixedSize(400, 400)
-        self.label.setPixmap(QPixmap(400, 400))
-        self.label.move(100, 100)
-
-    def circle(self):
-        x, y = randint(100, 300), randint(100, 300)
-        r = randint(10, 100)
-        painter = QPainter(self.label.pixmap())
-        pen = QPen()
-        pen.setWidth(3)
-        pen.setColor(QColor(255, 255, 0))
-        painter.setPen(pen)
-        painter.drawEllipse(x, y, r, r)
-        painter.end()
-        self.update()
+def circle(self):
+    x, y = randint(100, 300), randint(100, 300)
+    r = randint(10, 100)
+    painter = QPainter(ui.label.pixmap())
+    pen = QPen()
+    pen.setWidth(3)
+    pen.setColor(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
+    painter.setPen(pen)
+    painter.drawEllipse(x, y, r, r)
+    painter.end()
+    mw.update()
 
 
-if __name__ == '__main__':
-    import sys
+ui.pushButton.clicked.connect(circle)
 
-    app = QApplication(sys.argv)
-    w = Test()
-    w.show()
-    sys.exit(app.exec_())
+sys.exit(app.exec_())
