f576f543ec0e72b874f15f1c7f1019f45ace80b2
superGLSH <glebshu@gmail.com>
2021-11-22 18:26:37 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,38 @@
+﻿from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow, QLabel, QGridLayout
+from PyQt5.QtGui import QPainter, QPixmap, QPen, QColor
+from PyQt5.QtCore import Qt
+from PyQt5 import uic
+from random import randint
+
+
+class Test(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+
+        self.pushButton.clicked.connect(self.circle)
+
+        self.label.setFixedSize(400, 400)
+        self.label.setPixmap(QPixmap(400, 400))
+        self.label.move(100, 100)
+
+    def circle(self):
+        x, y = randint(100, 300), randint(100, 300)
+        r = randint(10, 100)
+        painter = QPainter(self.label.pixmap())
+        pen = QPen()
+        pen.setWidth(3)
+        pen.setColor(QColor(255, 255, 0))
+        painter.setPen(pen)
+        painter.drawEllipse(x, y, r, r)
+        painter.end()
+        self.update()
+
+
+if __name__ == '__main__':
+    import sys
+
+    app = QApplication(sys.argv)
+    w = Test()
+    w.show()
+    sys.exit(app.exec_())
